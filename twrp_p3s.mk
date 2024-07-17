34ad9749e3d7674e91983dd97d471ce0ad6288a4
Jamie Falconer <mrfluffyoven@gmail.com>
2024-07-17 09:31:34 +0100

    omni > twrp

--- twrp_p3s.mk
+++ twrp_p3s.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from p3s device
 $(call inherit-product, device/samsung/p3s/device.mk)
 
 PRODUCT_DEVICE := p3s
-PRODUCT_NAME := omni_p3s
+PRODUCT_NAME := twrp_p3s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G998B
 PRODUCT_MANUFACTURER := samsung
